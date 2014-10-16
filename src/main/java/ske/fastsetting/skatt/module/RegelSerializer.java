736ecdc86dd464e82e2e6f55940b6589de662a4d
eivindw <eivindw@gmail.com>
2014-10-16 09:08:54 +0200

    Fjern ubrukt import.

--- src/main/java/ske/fastsetting/skatt/module/RegelSerializer.java
+++ src/main/java/ske/fastsetting/skatt/module/RegelSerializer.java
@@ -3,7 +3,6 @@ package ske.fastsetting.skatt.module;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
-import ske.fastsetting.skatt.domene.KalkulerbarVerdi;
 import ske.fastsetting.skatt.domene.Regel;
 
 import java.io.IOException;
@@ -17,8 +16,8 @@ public class RegelSerializer extends StdSerializer<Regel> {
     @Override
     public void serialize(Regel value, JsonGenerator jgen, SerializerProvider provider) throws IOException {
         jgen.writeStartObject();
-        jgen.writeObjectField("navn", value.getKortnavnOgParagraf());
-        jgen.writeObjectField("uri", value.getLovdataUri());
+        jgen.writeObjectField("navn", value.kortnavnOgParagraf());
+        jgen.writeObjectField("uri", value.uri());
         jgen.writeEndObject();
     }
 }
